886d773a8bb80ebb970f4892bf0a14d75143871c
diego.rafael <diego.rafael@servicenet.com.br>
2024-09-25 22:58:54 -0300

    chore/refatoracao-completa-modulos-docs

--- provider.tf
+++ provider.tf
@@ -5,9 +5,17 @@ terraform {
       version = "~> 5.0"
     }
   }
+  required_version = ">= 1.9.5"
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  default_tags {
+    tags = {
+      Owner     = "diegodrk"
+      ManagedBy = "terraform"
+      Project   = "https://github.com/descomplicando-terraform/terraform-aws-Diego-Rafael_produto/"
+    }
+  }
 }
